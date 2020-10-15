ff9b4f71590d18173ae497630112f1064daa3e0d
LiJian <lijia@163.com>
2020-10-15 21:49:59 +0800

    add dubbo basic

---
+++ busi-api/src/main/java/com.enjoy.entity/OrderEntity.java
@@ -0,0 +1,54 @@
+package com.enjoy.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderEntity implements Serializable{
+
+    private String id;
+    private long money;
+    private String userId;
+    private int status = 0;
+    private List<ProductEntity> productlist = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public List<ProductEntity> getProductlist() {
+        return productlist;
+    }
+
+    public void setProductlist(List<ProductEntity> productlist) {
+        this.productlist = productlist;
+    }
+}
